9a1943c197cb1194812c424cb012456780a8b250
Juan Esteban Fernandez Campo <ranijuangaymer24@gmail.com>
2026-05-28 22:14:57 -0500

    remove: eliminar build script

--- build_exe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Build script para generar GlassTracker.exe (Windows)
-echo Construyendo GlassTracker...
-python -m pip install --upgrade pyinstaller
-python -m PyInstaller --noconfirm --onefile --windowed --add-data "tasks.json;." --name GlassTracker glass_tracker_gui.py
-echo Terminado. El ejecutable se encuentra en "dist\\GlassTracker.exe"
-echo Presiona una tecla para salir...
-pause >nul
